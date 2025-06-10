0537f2865e59cb0b3aed0e33f6e89c8a357d6895
aabccd021 <aabccd021@gmail.com>
2025-06-10 20:21:58 +0700

    Refactor tests: Add new tests, remove outdated ones, and rename files.

--- tests/default.nix
+++ tests/default.nix
@@ -13,17 +13,16 @@ let
     };
 in
 {
-  test01 = mkTest ./test01.nix;
-  test-simple-encrypted = mkTest ./simple-encrypted.nix;
-  test-reboot-after-create = mkTest ./reboot-after-create.nix;
-  test-reboot-after-restore = mkTest ./reboot-after-restore.nix;
-  test-reboot-after-backup = mkTest ./reboot-after-backup.nix;
-  test-reboot-after-destroy = mkTest ./reboot-after-destroy.nix;
-  test-reboot-all = mkTest ./reboot-all.nix;
-  test-reboot-all-canmount-on = mkTest ./reboot-all-canmount-on.nix;
-  test-reboot-all-canmount-noauto = mkTest ./reboot-all-canmount-noauto.nix;
-  test-reboot-all-multi-dataset = mkTest ./reboot-all-multi-dataset.nix;
+  test-empty = mkTest ./empty.nix;
   test-idempotent = mkTest ./idempotent.nix;
   test-multi-dataset-no-reboot = mkTest ./multi-dataset-no-reboot.nix;
-
+  test-reboot-after-backup = mkTest ./reboot-after-backup.nix;
+  test-reboot-after-create = mkTest ./reboot-after-create.nix;
+  test-reboot-after-destroy = mkTest ./reboot-after-destroy.nix;
+  test-reboot-after-restore = mkTest ./reboot-after-restore.nix;
+  test-reboot-all = mkTest ./reboot-all.nix;
+  test-reboot-all-canmount-noauto = mkTest ./reboot-all-canmount-noauto.nix;
+  test-reboot-all-canmount-on = mkTest ./reboot-all-canmount-on.nix;
+  test-reboot-all-multi-dataset = mkTest ./reboot-all-multi-dataset.nix;
+  test-simple-encrypted = mkTest ./simple-encrypted.nix;
 }
