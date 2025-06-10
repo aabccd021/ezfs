b2795a874b85880f35349349e5032e836db952d4
aabccd021 <aabccd021@gmail.com>
2025-06-10 10:40:54 +0700

    Refactor ZFS dataset mounting and user permissions.

--- nixosModules/default.nix
+++ nixosModules/default.nix
@@ -138,193 +138,207 @@ in
     };
   };
 
-  config.systemd = mapDataset (
-    ds: cfg:
-    let
-      opts = cfg.options // {
-        canmount = "noauto";
-      };
+  config = lib.mkMerge [
+    {
+      systemd = mapDataset (
+        ds: cfg:
+        let
+          opts = cfg.options // {
+            canmount = "noauto";
+          };
 
-      onetimeProperties = [
-        "encryption"
-        "casesensitivity"
-        "utf8only"
-        "normalization"
-        "volblocksize"
-        "pbkdf2iters"
-        "pbkdf2salt"
-        "keyformat"
+          onetimeProperties = [
+            "encryption"
+            "casesensitivity"
+            "utf8only"
+            "normalization"
+            "volblocksize"
+            "pbkdf2iters"
+            "pbkdf2salt"
+            "keyformat"
+          ];
+
+          updateOptions = builtins.removeAttrs opts onetimeProperties;
+
+          serviceName = "ezfs-" + formalName ds;
+
+          userAllows = lib.mapAttrs' (tds: tdsCfg: {
+            name = tdsCfg.user;
+            value = [
+              "send"
+              "hold"
+              "bookmark"
+            ];
+          }) cfg.pull-backup;
+
+        in
+        {
+          services.${serviceName} = {
+            description = "Mount ZFS dataset ${ds}";
+            restartIfChanged = true;
+            serviceConfig.Type = "oneshot";
+            after = [
+              "zfs-import.target"
+              "sops-install-secrets.service"
+            ];
+            path = [ "/run/booted-system/sw/" ];
+            enableStrictShellChecks = true;
+            environment.DATASET = ds;
+            environment.USER = cfg.user;
+            environment.GROUP = cfg.group;
+            environment.MOUNTPOINT = cfg.options.mountpoint;
+            script = ''
+              set -x
+              setOption() {
+                if [ "$(zfs get -H -o value "$1" "$DATASET")" != "$2" ]; then
+                  zfs set "$1=$2" "$DATASET"
+                fi
+              }
+
+              pool=$(echo "$DATASET" | cut -d'/' -f1)
+              if ! zpool list "$pool" >/dev/null 2>&1; then
+                zpool import "$pool"
+              fi
+
+              ${lib.concatStringsSep "\n" (lib.mapAttrsToList (n: v: "setOption ${n} ${v}") updateOptions)}
+
+              encryption=$(zfs get -H -o value encryption "$DATASET")
+              if [ "$encryption" != "off" ]; then
+                
+                keystatus=$(zfs get -H -o value keystatus "$DATASET")
+                if [ "$keystatus" != "available" ]; then
+                  zfs load-key "$DATASET"
+                fi
+              fi
+
+              if [ "$(zfs get -H -o value mounted "$DATASET")" != "yes" ]; then
+                zfs mount "$DATASET"
+              fi
+
+              chown "$USER":"$GROUP" "$MOUNTPOINT"
+
+              ${lib.concatStringsSep "\n" (
+                lib.mapAttrsToList (n: v: "zfs allow -u ${n} ${lib.concatStringsSep "," v} ${ds}") userAllows
+              )}
+
+            '';
+          };
+        }
+      );
+
+    }
+    {
+      sops = mapTarget (
+        { dsName, cfg, ... }:
+        {
+          sops.secrets.${secretName dsName} = cfg.privateKey // {
+            owner = config.services.syncoid.user;
+            group = config.services.syncoid.group;
+          };
+        }
+      );
+
+    }
+    {
+      services = lib.mkMerge [
+        (mapTarget (
+          { dsName, cfg }:
+          {
+            syncoid.enable = true;
+            syncoid.commands."pull-backup-${formalName dsName}" = {
+              sshKey = sshKey dsName;
+              source = source dsName cfg;
+              target = cfg.targetDataset;
+              # w = send dataset as is, not decrypted on transfer when the source dataset is encrypted
+              sendOptions = "w";
+              # u = don't mount the dataset after restore
+              recvOptions = "u o canmount=off o mountpoint=none o keylocation=file:///dev/null";
+              extraArgs = cfg.pullExtraArgs ++ [
+                "--sshoption='StrictHostKeyChecking=yes'"
+                "--sshoption='UserKnownHostsFile=${knownHost cfg}'"
+                # don't create new snapshot on source before backup, since we already created it with sanoid
+                "--no-sync-snap"
+                # no cons of using this,
+                # don't forget to run `zfs allow -u <username> bookmark <pool>/<dataset>` on the remote host
+                "--create-bookmark"
+              ];
+            };
+
+          }
+        ))
       ];
 
-      updateOptions = builtins.removeAttrs opts onetimeProperties;
-
-      serviceName = "ezfs-" + formalName ds;
-
-      userAllows = lib.mapAttrs' (tds: tdsCfg: {
-        name = tdsCfg.user;
-        value = [
-          "send"
-          "hold"
-          "bookmark"
-        ];
-      }) cfg.pull-backup;
-
-    in
+    }
     {
-      services.${serviceName} = {
-        description = "Mount ZFS dataset ${ds}";
-        restartIfChanged = true;
-        serviceConfig.Type = "oneshot";
-        after = [
-          "zfs-import.target"
-          "sops-install-secrets.service"
-        ];
-        path = [ "/run/booted-system/sw/" ];
-        enableStrictShellChecks = true;
-        environment.DATASET = ds;
-        environment.USER = cfg.user;
-        environment.GROUP = cfg.group;
-        environment.MOUNTPOINT = cfg.options.mountpoint;
-        script = ''
-          set -x
-          setOption() {
-            if [ "$(zfs get -H -o value "$1" "$DATASET")" != "$2" ]; then
-              zfs set "$1=$2" "$DATASET"
-            fi
+      environment = lib.mkMerge [
+        (mapTarget (
+          { dsName, cfg }:
+          {
+            systemPackages = [
+              (pkgs.writeShellApplication {
+                name = "syncoid-pull-restore-${formalName dsName}";
+                runtimeInputs = [ config.services.syncoid.package ];
+                text = ''
+                  # TODO: check if source dataset exists before running this script
+                  # recvoptions u: Prevent auto mounting the dataset after restore. Just mount it manually.
+                  exec syncoid \
+                    ${lib.optionalString (cfg.restoreExtraArgs != [ ]) (lib.escapeShellArg cfg.restoreExtraArgs)} \
+                    --sshkey ${sshKey dsName} \
+                    --sshoption='StrictHostKeyChecking=yes' \
+                    --sshoption='UserKnownHostsFile=${knownHost cfg}' \
+                    --no-sync-snap \
+                    --no-privilege-elevation \
+                    --sendoptions="w" \
+                    --recvoptions="u" \
+                    ${cfg.targetDataset} \
+                    ${source dsName cfg}
+                '';
+              })
+            ];
           }
+        ))
+        (mapDataset (
+          dsName: cfg: {
+            systemPackages = [
+              pkgs.mbuffer
 
-          pool=$(echo "$DATASET" | cut -d'/' -f1)
-          if ! zpool list "$pool" >/dev/null 2>&1; then
-            zpool import "$pool"
-          fi
+              (pkgs.writeShellApplication {
+                name = "ezfs-create-${formalName dsName}";
+                runtimeInputs = [ "/run/booted-system/sw" ];
+                text = ''
+                  zfs create -u ${
+                    lib.concatStringsSep " " (lib.mapAttrsToList (n: v: "-o ${n}=${v}") cfg.options)
+                  } ${dsName}
+                '';
+              })
+            ];
+          }
+        ))
+      ];
 
-          ${lib.concatStringsSep "\n" (lib.mapAttrsToList (n: v: "setOption ${n} ${v}") updateOptions)}
-
-          encryption=$(zfs get -H -o value encryption "$DATASET")
-          if [ "$encryption" != "off" ]; then
-            
-            keystatus=$(zfs get -H -o value keystatus "$DATASET")
-            if [ "$keystatus" != "available" ]; then
-              zfs load-key "$DATASET"
-            fi
-          fi
-
-          if [ "$(zfs get -H -o value mounted "$DATASET")" != "yes" ]; then
-            zfs mount "$DATASET"
-          fi
-
-          chown "$USER":"$GROUP" "$MOUNTPOINT"
-
-          ${lib.concatStringsSep "\n" (
-            lib.mapAttrsToList (n: v: "zfs allow -u ${n} ${lib.concatStringsSep "," v} ${ds}") userAllows
-          )}
-
-        '';
-      };
     }
-  );
-
-  config.sops = mapTarget (
-    { dsName, cfg, ... }:
     {
-      secrets.${secretName dsName} = cfg.privateKey // {
-        owner = config.services.syncoid.user;
-        group = config.services.syncoid.group;
-      };
+      users = mapSource (
+        { cfg, ... }:
+        {
+          users.${cfg.user} = {
+            isNormalUser = true;
+            openssh.authorizedKeys.keys = [
+              cfg.publicKey
+            ];
+          };
+        }
+      );
+
     }
-  );
-
-  config.services = lib.mkMerge [
-    (mapTarget (
-      { dsName, cfg }:
-      {
-        syncoid.enable = true;
-        syncoid.commands."pull-backup-${formalName dsName}" = {
-          sshKey = sshKey dsName;
-          source = source dsName cfg;
-          target = cfg.targetDataset;
-          # w = send dataset as is, not decrypted on transfer when the source dataset is encrypted
-          sendOptions = "w";
-          # u = don't mount the dataset after restore
-          recvOptions = "u o canmount=off o mountpoint=none o keylocation=file:///dev/null";
-          extraArgs = cfg.pullExtraArgs ++ [
-            "--sshoption='StrictHostKeyChecking=yes'"
-            "--sshoption='UserKnownHostsFile=${knownHost cfg}'"
-            # don't create new snapshot on source before backup, since we already created it with sanoid
-            "--no-sync-snap"
-            # no cons of using this,
-            # don't forget to run `zfs allow -u <username> bookmark <pool>/<dataset>` on the remote host
-            "--create-bookmark"
-          ];
-        };
-
-      }
-    ))
-  ];
-
-  config.environment = lib.mkMerge [
-    (mapTarget (
-      { dsName, cfg }:
-      {
-        systemPackages = [
-          (pkgs.writeShellApplication {
-            name = "syncoid-pull-restore-${formalName dsName}";
-            runtimeInputs = [ config.services.syncoid.package ];
-            text = ''
-              # TODO: check if source dataset exists before running this script
-              # recvoptions u: Prevent auto mounting the dataset after restore. Just mount it manually.
-              exec syncoid \
-                ${lib.optionalString (cfg.restoreExtraArgs != [ ]) (lib.escapeShellArg cfg.restoreExtraArgs)} \
-                --sshkey ${sshKey dsName} \
-                --sshoption='StrictHostKeyChecking=yes' \
-                --sshoption='UserKnownHostsFile=${knownHost cfg}' \
-                --no-sync-snap \
-                --no-privilege-elevation \
-                --sendoptions="w" \
-                --recvoptions="u" \
-                ${cfg.targetDataset} \
-                ${source dsName cfg}
-            '';
-          })
-        ];
-      }
-    ))
-    (mapDataset (
-      dsName: cfg: {
-        systemPackages = [
-          pkgs.mbuffer
-
-          (pkgs.writeShellApplication {
-            name = "ezfs-create-${formalName dsName}";
-            runtimeInputs = [ "/run/booted-system/sw" ];
-            text = ''
-              zfs create -u ${
-                lib.concatStringsSep " " (lib.mapAttrsToList (n: v: "-o ${n}=${v}") cfg.options)
-              } ${dsName}
-            '';
-          })
-        ];
-      }
-    ))
-  ];
-
-  config.users = mapSource (
-    { cfg, ... }:
     {
-      users.${cfg.user} = {
-        isNormalUser = true;
-        openssh.authorizedKeys.keys = [
-          cfg.publicKey
-        ];
-      };
-    }
-  );
+      assertions = [
+        {
+          assertion = config.services.openssh.hostKeys != [ ];
+          message = "services.openssh.hostKeys must be set for ezfs to work";
+        }
+      ];
 
-  config.assertions = [
-    {
-      assertion = config.services.openssh.hostKeys != [ ];
-      message = "services.openssh.hostKeys must be set for ezfs to work";
     }
   ];
-
 }
