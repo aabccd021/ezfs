a6a1704a48571e9f5a6c176f5d2ad8bdf309cf9f
aabccd021 <aabccd021@gmail.com>
2026-03-21 20:20:29 +0700

    Add test-push-multi-dataset test
    Test push backup with multiple datasets and dependencies,
    verifying that dataset mounting order respects dependsOn
    and data is correctly restored to the dependent dataset.

--- tests/default.nix
+++ tests/default.nix
@@ -18,6 +18,7 @@ in
   test-pull-encrypted = mkTest ./test-pull-encrypted.nix;
   test-push-encrypted = mkTest ./test-push-encrypted.nix;
   test-push-idempotent = mkTest ./test-push-idempotent.nix;
+  test-push-multi-dataset = mkTest ./test-push-multi-dataset.nix;
   test-push-reboot-all = mkTest ./test-push-reboot-all.nix;
   test-push-states = mkTest ./test-push-states.nix;
 }
---
+++ tests/test-push-multi-dataset.nix
@@ -0,0 +1,166 @@
+inputs:
+let
+  mock-secrets = inputs.mock-secrets-nix.lib.secrets;
+  sharedModule =
+    { config, ... }:
+    {
+
+      ezfs = {
+        hosts = {
+          "76219b03" = {
+            publicKey = mock-secrets.ed25519.bob.public;
+            privateKey = {
+              sopsFile = config.sops-mock.secrets.sshd_private_key.sopsFile;
+              key = "sshd_private_key";
+            };
+          };
+        };
+        datasets.myshallow = {
+          name = "spool/shallow";
+          hostId = "9b037621";
+          options = {
+            mountpoint = "/shallow";
+          };
+        };
+        datasets.myfoo = {
+          dependsOn = [ "myshallow" ];
+          name = "spool/foo";
+          hostId = "9b037621";
+          options = {
+            encryption = "on";
+            keyformat = "passphrase";
+            keylocation = "file:///run/encryption_key.txt";
+            mountpoint = "/shallow/foo";
+          };
+        };
+        push-backups.mybackup = {
+          targetDatasetName = "vpool/foo_backup";
+          sourceDatasetId = "myfoo";
+          hostId = "76219b03";
+          host = "vps";
+          user = "mybackupuser";
+          publicKey = mock-secrets.ed25519.alice.public;
+          privateKey = {
+            key = "backup_ssh_key";
+            sopsFile = config.sops-mock.secrets.backup_private_key.sopsFile;
+          };
+        };
+      };
+
+      # required for test only
+      virtualisation.emptyDiskImages = [ 4096 ]; # add /dev/vdb
+      sops.validateSopsFiles = false; # Required for allow-import-from-derivation = false;
+      sops.age.keyFile = config.sops-mock.age.keyFile;
+      imports = [ inputs.sops-nix-mock.nixosModules.default ];
+    };
+in
+
+{
+  name = "push-multi-dataset";
+
+  nodes.server =
+    { config, ... }:
+    {
+      imports = [
+        inputs.sops-nix.nixosModules.default
+        inputs.ezfs.nixosModules.default
+        sharedModule
+      ];
+
+      # required for zfs
+      networking.hostId = "9b037621";
+
+      # simulate putting secrets
+      boot.initrd.postDeviceCommands = ''
+        echo "encryption key" > /run/encryption_key.txt
+        chmod 400 /run/encryption_key.txt
+      '';
+
+      # Required for test only
+      systemd.services."zfs-import-spool".serviceConfig.TimeoutStartSec = "1s";
+      sops-mock = {
+        enable = true;
+        secrets.backup_private_key.value = mock-secrets.ed25519.alice.private;
+        secrets.backup_private_key.key = "backup_ssh_key";
+      };
+    };
+
+  nodes.vps = {
+    imports = [
+      inputs.sops-nix.nixosModules.default
+      inputs.ezfs.nixosModules.default
+      sharedModule
+    ];
+
+    networking.hostId = "76219b03";
+
+    ezfs.push-backups.mybackup.enable = true;
+
+    # Required for test only
+    systemd.services."zfs-import-vpool".serviceConfig.TimeoutStartSec = "1s";
+    sops-mock = {
+      enable = true;
+      secrets.sshd_private_key.value = mock-secrets.ed25519.bob.private;
+      secrets.sshd_private_key.key = "sshd_private_key";
+    };
+  };
+
+  testScript =
+    { nodes, ... }:
+    ''
+      start_all()
+
+      # create hello before mounting anything
+      server.wait_for_unit("multi-user.target")
+      server.succeed("mkdir -p /shallow/foo")
+      server.succeed("echo 'not zfs' > /shallow/foo/hello.txt")
+
+      # create zpools
+      server.succeed("zpool create spool /dev/vdb")
+      vps.succeed("zpool create vpool /dev/vdb")
+
+      # create datasets
+      server.succeed("ezfs-create-myfoo")
+      server.succeed("ezfs-create-myshallow")
+
+      # setup on vps
+      vps.succeed("systemctl start --wait ezfs-setup-push-backup-mybackup")
+
+      # setup datasets (myfoo depends on myshallow)
+      server.succeed("systemctl start --wait ezfs-setup-dataset-myfoo")
+
+      # insert data
+      server.succeed("echo 'foo' > /shallow/foo/hello.txt")
+
+      # backup
+      server.succeed("systemctl start --wait sanoid")
+      server.succeed("systemctl start --wait ${nodes.server.ezfs.push-backups.mybackup.backupService}")
+
+      # simulate data loss
+      server.succeed("test -f /shallow/foo/hello.txt")
+      server.succeed("zfs destroy -r spool/foo")
+      server.fail("test -f /shallow/foo/hello.txt")
+
+      # insert to shallow dataset
+      server.succeed("echo 'shallow' > /shallow/foo/hello.txt")
+
+      # restore
+      vps.succeed("ezfs-prepare-restore-push-backup-mybackup")
+      server.succeed("ezfs-restore-push-backup-mybackup")
+
+      # setup dataset
+      server.succeed("systemctl start --wait ezfs-setup-dataset-myfoo")
+
+      # assert foo dataset data is restored
+      server.succeed("test -f /shallow/foo/hello.txt")
+      server.succeed("cat /shallow/foo/hello.txt | grep '^foo$'")
+
+      # unmount foo and check shallow dataset
+      server.succeed("zfs unmount spool/foo")
+      server.succeed("cat /shallow/foo/hello.txt | grep '^shallow$'")
+
+      # unmount shallow and check non-zfs hello.txt
+      server.succeed("zfs unmount spool/shallow")
+      server.succeed("cat /shallow/foo/hello.txt | grep '^not zfs$'")
+    '';
+}
